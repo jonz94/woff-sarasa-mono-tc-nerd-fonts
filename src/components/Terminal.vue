63bc2300b23e90a607de616c16e587fce5f01aa1
jonz94 <jody16888@gmail.com>
2021-10-28 11:18:33 +0800

    ✅ fix: use `inline-flex` in terminal component to address layout problem
    - fix terminal layout problem
    - make terminal prompt as standalone component

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,77 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Prompt from './TerminalPrompt.vue'
+</script>
 
 <template>
   <div
-    class="flex flex-col p-2 h-64 text-xl rounded"
+    class="flex flex-col p-2 h-64 text-xl leading-tight rounded"
     style="background: #121212"
   >
-    <p>
-      <span class="pl-[1ch]" style="background: #343a43; color: #e5c07b">
-        jonz94@jonzWin11WS
-      </span>
-      <span style="background: #dcdfe4; color: #343a43"></span>
-      <span class="pr-[1ch]" style="background: #dcdfe4; color: #343a43">
-        
-      </span>
-      <span style="background: #61afef; color: #dcdfe4"></span>
-      <span class="pr-[1ch]" style="background: #61afef; color: #343a43">
-        
-      </span>
-      <span style="background: #61afef; color: #343a43"> ~/dotfiles </span>
-      <span style="background: #a7cb8b; color: #61afef"></span>
-      <span style="background: #a7cb8b; color: #343a43">   main ± </span>
-      <span style="background: #121212; color: #a7cb8b"></span>
-      <span style="background: #121212; color: #121212">spacespace</span>
-      <span style="background: #121212; color: #343a43"></span>
-      <span class="pr-[1ch]" style="background: #343a43; color: #a7cb8b">
-        
-      </span>
-      <span style="background: #343a43; color: #dcdfe4"></span>
-      <span class="pr-[2ch]" style="background: #343a43; color: #dcdfe4">
-        13.17G 
-      </span>
+    <p class="inline-flex">
+      <Prompt />
     </p>
-    <p>
-      <span style="background: #dcdfe4; color: #dcdfe4">s</span>
+    <p class="inline-flex">
+      <span class="w-[1ch]" style="background: #dcdfe4; color: #dcdfe4"></span>
       <span style="background: #121212; color: #dcdfe4"> </span>
-      <span style="background: #121212; color: #a7cb8b">echo</span>
+      <span class="px-[1ch]" style="background: #121212; color: #a7cb8b">
+        echo
+      </span>
       <span style="background: #121212; color: white">
         Hello from terminal!
       </span>
     </p>
-    <p>
+    <p class="inline-flex">
       <span style="background: #121212; color: white">
         Hello from terminal!
       </span>
     </p>
-    <p>
-      <span class="pl-[1ch]" style="background: #343a43; color: #e5c07b">
-        jonz94@jonzWin11WS
-      </span>
-      <span style="background: #dcdfe4; color: #343a43"></span>
-      <span class="pr-[1ch]" style="background: #dcdfe4; color: #343a43">
-        
-      </span>
-      <span style="background: #61afef; color: #dcdfe4"></span>
-      <span class="pr-[1ch]" style="background: #61afef; color: #343a43">
-        
-      </span>
-      <span style="background: #61afef; color: #343a43"> ~/dotfiles </span>
-      <span style="background: #a7cb8b; color: #61afef"></span>
-      <span style="background: #a7cb8b; color: #343a43">   main ± </span>
-      <span style="background: #121212; color: #a7cb8b"></span>
-      <span style="background: #121212; color: #121212">spacespace</span>
-      <span style="background: #121212; color: #343a43"></span>
-      <span class="pr-[1ch]" style="background: #343a43; color: #a7cb8b">
-        
-      </span>
-      <span style="background: #343a43; color: #dcdfe4"></span>
-      <span class="pr-[2ch]" style="background: #343a43; color: #dcdfe4">
-        13.13G 
-      </span>
+    <p class="inline-flex">
+      <Prompt />
     </p>
-    <p>
-      <span style="background: #dcdfe4; color: #dcdfe4">s</span>
+    <p class="inline-flex">
+      <span class="w-[1ch]" style="background: #dcdfe4; color: #dcdfe4"></span>
       <span style="background: #121212; color: #dcdfe4"> </span>
     </p>
   </div>
---
+++ src/components/TerminalPrompt.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <span class="px-[1ch]" style="background: #343a43; color: #e5c07b">
+    jonz94@jonzWin11WS
+  </span>
+  <span style="background: #dcdfe4; color: #343a43"></span>
+  <span class="pl-[1ch] pr-[2ch]" style="background: #dcdfe4; color: #343a43">
+    
+  </span>
+  <span style="background: #61afef; color: #dcdfe4"></span>
+  <span class="px-[1ch]" style="background: #61afef; color: #343a43"> </span>
+  <span class="px-[1ch]" style="background: #61afef; color: #343a43">
+    ~/dotfiles
+  </span>
+  <span style="background: #a7cb8b; color: #61afef"></span>
+  <span class="px-[1ch]" style="background: #a7cb8b; color: #343a43">
+      main ±
+  </span>
+  <span style="background: #121212; color: #a7cb8b"></span>
+  <span class="w-[20ch]" style="background: #121212; color: #121212"></span>
+  <span style="background: #121212; color: #343a43"></span>
+  <span class="pl-[1ch] pr-[2ch]" style="background: #343a43; color: #a7cb8b">
+    
+  </span>
+  <span style="background: #343a43; color: #dcdfe4"></span>
+  <span class="pl-[1ch] pr-[2ch]" style="background: #343a43; color: #dcdfe4">
+    13.17G 
+  </span>
+</template>
