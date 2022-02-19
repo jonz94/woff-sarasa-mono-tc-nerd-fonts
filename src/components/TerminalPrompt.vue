a4aeae3072267366e7748bbdfdead8ca901b30b3
jonz94 <jody16888@gmail.com>
2022-02-19 14:11:05 +0800

    ✨ feat: tweak icons inside terminal prompt

--- src/components/TerminalPrompt.vue
+++ src/components/TerminalPrompt.vue
@@ -14,9 +14,11 @@
     ~/dotfiles
   </span>
   <span style="background: #a7cb8b; color: #61afef"></span>
+  <span class="px-[1ch]" style="background: #a7cb8b; color: #343a43"> </span>
   <span class="px-[1ch]" style="background: #a7cb8b; color: #343a43">
-      main ±
+     main
   </span>
+  <span class="pr-[2ch]" style="background: #a7cb8b; color: #343a43"> </span>
   <span style="background: #121212; color: #a7cb8b"></span>
   <span
     class="flex-auto lg:flex-initial min-w-[4ch] lg:w-[20ch]"
