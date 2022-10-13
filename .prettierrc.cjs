834c204bcb75b139d27a8b016e3d9159bdd29c67
jonz94 <jody16888@gmail.com>
2022-10-13 08:52:44 +0800

    🔧 build(config): tweak code formatter settings

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,12 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   printWidth: 80,
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
+
+  plugins: [
+    require('prettier-plugin-packagejson'),
+    require('prettier-plugin-tailwindcss'),
+  ],
 }
