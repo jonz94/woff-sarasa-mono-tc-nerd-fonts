b4c99ebc4cde221ed8bcf94683816c500c24cb6a
jonz94 <jody16888@gmail.com>
2022-12-11 17:07:59 +0800

    ✅ hotfix: resolve "LinkError: WebAssembly.instantiate()" issue

--- copy-shiki.sh
+++ copy-shiki.sh
@@ -9,3 +9,7 @@ mkdir -p public/shiki
 cp -r -L node_modules/shiki/dist public/shiki/dist
 cp -r -L node_modules/shiki/languages public/shiki/languages
 cp -r -L node_modules/shiki/themes public/shiki/themes
+
+# resolve "LinkError: WebAssembly.instantiate()" issue
+# credit: https://github.com/shikijs/shiki/issues/382#issuecomment-1340178768
+cp -L node_modules/vscode-oniguruma/release/onig.wasm public/shiki/dist
